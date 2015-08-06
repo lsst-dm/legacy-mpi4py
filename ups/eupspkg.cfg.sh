60bf4efc7bcb00cba7eae9038105dd1f1d79fff6
Jim Bosch <jbosch@astro.princeton.edu>
2015-08-06 16:18:24 -0400

    Add table file and build scripts, translated from HSC.

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,10 @@
+# EupsPkg config file. Sourced by 'eupspkg'
+
+prep()
+{
+    default_prep
+    cat >> setup.cfg <<EOF
+    [build_ext]
+    define = PyMPI_MISSING_MPI_Type_create_f90_integer, PyMPI_MISSING_MPI_Type_create_f90_real, PyMPI_MISSING_MPI_Type_create_f90_complex
+    EOF
+}
