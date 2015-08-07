af21c22d8f0ea964d7766e9c964657ba5d70f69c
Jim Bosch <jbosch@astro.princeton.edu>
2015-08-07 13:50:28 -0400

    Fix indentation that breaks here-document.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -4,7 +4,7 @@ prep()
 {
     default_prep
     cat >> setup.cfg <<EOF
-    [build_ext]
-    define = PyMPI_MISSING_MPI_Type_create_f90_integer, PyMPI_MISSING_MPI_Type_create_f90_real, PyMPI_MISSING_MPI_Type_create_f90_complex
-    EOF
+[build_ext]
+define = PyMPI_MISSING_MPI_Type_create_f90_integer, PyMPI_MISSING_MPI_Type_create_f90_real, PyMPI_MISSING_MPI_Type_create_f90_complex
+EOF
 }
